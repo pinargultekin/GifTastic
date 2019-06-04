6c100746b804b51a466dcd84462c7079a8b3d492
29bucuk <pinargultekin@yahoo.com>
2019-06-04 19:09:28 -0400

    input submit function added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,42 @@
-//<a href="https://www.freepik.com/free-photos-vectors/love">Love vector created by freepik - www.freepik.com</a>
 $(document).ready(function () {
+
+    //VARIABLE
+    var currentMood = ["Happy", "Excited", "Energetic", "Angry", "Joyful", "Melancholy", "Stressed", "Irritated"];
+
+
+    //FUNCTION  
+    function buttons() {
+
+        // $("#buttons").empty();
+
+        for (var i = 0; i < currentMood.length; i++) {
+            var btn = $("<button>");
+            btn.addClass("searchButton");
+            btn.attr("data-mood", currentMood[i]);
+            btn.text(currentMood[i]);
+            $("#buttons").append(btn);
+
+        };
+    };
+
+    // Adding mood buttons using submit button
+    $("#addMood").on("click", function (event) {
+        //   $("#user-input").empty();
+        event.preventDefault();
+        var userInput = $("#user-input").val().trim();
+        currentMood.push(userInput);
+        $("#buttons").append("<button class='searchButton' data-mood= " + userInput + ">" + userInput + "</button>");
+
+
+    });
+
     $("#gifs").hide();
-    $("input").on("click", function () {
+
+    // click event for search button 
+    $(document).on("click", ".searchButton", function () {
         $("#gifs").show();
-        
+        $("#gifs").empty();
+
         var mood = $(this).attr("data-mood");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=9Li299K6c8k8VCHxN38Q4Uht6Bkp3XXG&limit=10";
 
@@ -13,25 +46,50 @@ $(document).ready(function () {
         })
 
             .then(function (response) {
-                console.log(queryURL);
-                console.log(response);
 
                 var results = response.data;
 
-                for (var i = 0; i <results.length; i++) {
+                for (var i = 0; i < results.length; i++) {
                     var moodDiv = $("<div>");
 
                     var p = $("<p>").text("Rating: " + results[i].rating);
 
                     var moodImage = $("<img>");
                     moodImage.attr("src", results[i].images.fixed_height.url);
+                    moodImage.attr("data-animate", results[i].images.fixed_height.url);
+                    moodImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    moodImage.attr("data-state", "still");
+                    moodImage.attr("class", "gif");
 
                     moodDiv.append(moodImage);
                     moodDiv.append(p);
 
                     $("#gifs").prepend(moodDiv);
-                    
+
                 };
             });
+
     });
-})
+
+
+    buttons();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//<a href="https://www.freepik.com/free-photos-vectors/love">Love vector created by freepik - www.freepik.com</a>
---
+++ pseudo.md
@@ -0,0 +1,9 @@
+#GifTastic Pseudo Code
+
+1. Create buttons based on elements in the array
+2. When button is clicked display gifs accordingly the button name
+3. Create a form that user can write a key word.
+4. Get the value of text box input and turn it to a button
+5. When created button clicked it behaves like the other buttons
+6. When gifs displayed on the screen it should be still
+7. Once users click it animates, and with another click it turns still state.
